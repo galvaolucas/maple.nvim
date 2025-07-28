e3cbb2e2764b0dc1575f17aba450c76f69a53bfc
Lucas  Galvão <lucasgalvao@Lucass-MacBook-Air.local>
2025-07-28 19:03:39 -0300

    trying to fix lualine bug pt.4

--- lua/maple/lualine.lua
+++ lua/maple/lualine.lua
@@ -1,42 +1,53 @@
-local maple = require("maple.pallete")
-local colors = maple.colors
+-- Bubbles config for lualine
+-- Author: lokesh-krishna
+-- MIT license, see LICENSE for more details.
 
-local theme = {
+-- stylua: ignore
+local colors = {
+  blue   = '#80a0ff',
+  cyan   = '#79dac8',
+  black  = '#080808',
+  white  = '#c6c6c6',
+  red    = '#ff5189',
+  violet = '#d183e8',
+  grey   = '#303030',
+}
+
+local bubbles_theme = {
 	normal = {
-		a = { fg = colors.black, bg = colors.magenta, gui = "bold" },
-		b = { fg = colors.bright_white, bg = colors.black },
-		c = { fg = colors.fg, bg = colors.bg },
+		a = { fg = colors.black, bg = colors.violet },
+		b = { fg = colors.white, bg = colors.grey },
+		c = { fg = colors.white },
 	},
-	insert = {
-		a = { fg = colors.black, bg = colors.blue, gui = "bold" },
-	},
-	visual = {
-		a = { fg = colors.black, bg = colors.cyan, gui = "bold" },
-	},
-	replace = {
-		a = { fg = colors.black, bg = colors.red, gui = "bold" },
-	},
+
+	insert = { a = { fg = colors.black, bg = colors.blue } },
+	visual = { a = { fg = colors.black, bg = colors.cyan } },
+	replace = { a = { fg = colors.black, bg = colors.red } },
+
 	inactive = {
-		a = { fg = colors.fg, bg = colors.black, gui = "bold" },
-		b = { fg = colors.fg, bg = colors.black },
-		c = { fg = colors.fg, bg = colors.bg },
+		a = { fg = colors.white, bg = colors.black },
+		b = { fg = colors.white, bg = colors.black },
+		c = { fg = colors.white },
 	},
 }
 
--- Auto setup lualine on require
 require("lualine").setup({
 	options = {
-		theme = theme,
+		theme = bubbles_theme,
 		component_separators = "",
 		section_separators = { left = "", right = "" },
 	},
 	sections = {
 		lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
 		lualine_b = { "filename", "branch" },
-		lualine_c = { "%=" },
+		lualine_c = {
+			"%=", --[[ add your center components here in place of this comment ]]
+		},
 		lualine_x = {},
 		lualine_y = { "filetype", "progress" },
-		lualine_z = { { "location", separator = { right = "" }, left_padding = 2 } },
+		lualine_z = {
+			{ "location", separator = { right = "" }, left_padding = 2 },
+		},
 	},
 	inactive_sections = {
 		lualine_a = { "filename" },
