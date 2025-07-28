44b14846d5defdb167b6b1d997c19dad9f73e131
Lucas  Galvão <lucasgalvao@Lucass-MacBook-Air.local>
2025-07-28 18:35:24 -0300

    improving lualine

--- lua/maple/lualine.lua
+++ lua/maple/lualine.lua
@@ -4,7 +4,7 @@ local colors = maple.colors
 local theme = {
 	normal = {
 		a = { fg = colors.black, bg = colors.magenta, gui = "bold" },
-		b = { fg = colors.fg, bg = colors.columbia_blue },
+		b = { fg = colors.bright_yellow, bg = colors.black },
 		c = { fg = colors.fg, bg = colors.bg },
 	},
 	insert = {
