c14cc0b48d24cad95576ce224d2eb0e53a206d6e
Lucas  Galvão <lucasgalvao@Lucass-MacBook-Air.local>
2025-07-28 16:15:16 -0300

    new pallette

--- lua/maple/theme.lua
+++ lua/maple/theme.lua
@@ -1,31 +1,31 @@
 local maple = {}
 
 maple.colors = {
-	bg = "#1e1e1f",
-	fg = "#cbd5e1",
-	red = "#edabab",
-	green = "#a4dfae",
-	yellow = "#eecfa0",
-	blue = "#8fc7ff",
-	magenta = "#d2ccff",
-	cyan = "#a1e8e5",
-	white = "#f3f2f2",
-	black = "#333333",
-	pink = "#f3b8ff",
+	bg = "#1e1e2e",
+	fg = "#cdd6f4",
+	red = "#f38ba8",
+	green = "#a6e3a1",
+	yellow = "#f9e2af",
+	blue = "#89b4fa",
+	magenta = "#cba6f7",
+	cyan = "#94e2d5",
+	white = "#ffffff",
+	black = "#181825",
+	pink = "#f5c2e7",
 
-	bright_black = "#666666",
-	bright_red = "#ffc4c4",
-	bright_green = "#dbf195",
-	bright_yellow = "#ffe8b9",
-	bright_blue = "#a8e0ff",
-	bright_magenta = "#ebe5ff",
-	bright_cyan = "#bafffe",
-	bright_white = "#ffffff",
+	bright_black = "#313244",
+	bright_red = "#eba0ac",
+	bright_green = "#b5e8b0",
+	bright_yellow = "#f2cdcd",
+	bright_blue = "#89dceb",
+	bright_magenta = "#d0bfff",
+	bright_cyan = "#b5f4e6",
+	bright_white = "#f5f5f5",
 
-	cursor = "#ebe5ff",
-	selection = "#cbd5e1",
-	comment = "#999999",
-	border = "#44444b",
+	cursor = "#cba6f7",
+	selection = "#585b70",
+	comment = "#7f849c",
+	border = "#45475a",
 }
 
 local function hi(name, opts)
@@ -53,7 +53,7 @@ function maple.setup(opts)
 
 	hi("Normal", { fg = c.fg, bg = c.bg })
 	hi("Cursor", { fg = c.bg, bg = c.cursor })
-	hi("Visual", { bg = c.bg })
+	hi("Visual", { bg = c.selection })
 	hi("LineNr", { fg = c.comment })
 	hi("CursorLineNr", { fg = c.white, bold = true })
 	hi("Comment", { fg = c.comment, italic = true })
