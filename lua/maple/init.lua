a7d8f873a20d77c9f8984147e31a86864776ed43
Lucas  Galvão <lucasgalvao@Lucass-MacBook-Air.local>
2025-07-29 12:12:25 -0300

    improving lualine code

--- lua/maple/init.lua
+++ lua/maple/init.lua
@@ -4,6 +4,8 @@ local M = {}
 
 function M.setup(opts)
 	require("maple.theme").setup(opts or {}, require("maple.palette").colors(opts.theme or "autumn"))
+	local lualine = require("maple.lualine")
+	lualine.load_lualine(opts.theme)
 end
 
 return M
--- lua/maple/lualine.lua
+++ lua/maple/lualine.lua
@@ -1,103 +1,109 @@
 local maple = require("maple")
-local theme = maple.colors(maple.opts.theme)
 
-local colors = {
-	blue = theme.colors.blue,
-	cyan = theme.colors.cyan,
-	black = theme.colors.bright_black,
-	white = theme.colors.white,
-	red = theme.colors.red,
-	violet = theme.colors.magenta,
-	bright_magenta = theme.colors.bright_magenta,
-	brown = theme.colors.brown,
-}
-
-local function clock()
-	return "  " .. os.date("%H:%M")
-end
-
-local transparent = false
-if maple and maple.config then
-	transparent = maple.opts.transparent or false
-end
-
-local maple_theme = {}
-
-if transparent then
-	maple_theme = {
-		normal = {
-			a = { fg = colors.black, bg = colors.white, gui = "bold" },
-			b = { fg = colors.black, bg = colors.bright_magenta },
-			c = { fg = colors.black, bg = colors.bright_magenta },
-		},
-		insert = { a = { fg = colors.black, bg = nil } },
-		visual = { a = { fg = colors.black, bg = nil } },
-		replace = { a = { fg = colors.black, bg = nil } },
-		inactive = {
-			a = { fg = colors.white, bg = nil, gui = "bold" },
-			b = { fg = colors.white, bg = nil },
-			c = { fg = colors.white },
-		},
+load_lualine = function(theme)
+	local theme = maple.colors(theme)
+	local colors = {
+		blue = theme.colors.blue,
+		cyan = theme.colors.cyan,
+		black = theme.colors.bright_black,
+		white = theme.colors.white,
+		red = theme.colors.red,
+		violet = theme.colors.magenta,
+		bright_magenta = theme.colors.bright_magenta,
+		brown = theme.colors.brown,
 	}
-else
-	maple_theme = {
-		normal = {
-			a = { fg = colors.black, bg = colors.white, gui = "bold" },
-			b = { fg = colors.white, bg = colors.brown },
-			c = { fg = colors.white },
+
+	local function clock()
+		return "  " .. os.date("%H:%M")
+	end
+
+	local transparent = false
+	if maple and maple.config then
+		transparent = maple.opts.transparent or false
+	end
+
+	local maple_theme = {}
+
+	if transparent then
+		maple_theme = {
+			normal = {
+				a = { fg = colors.black, bg = colors.white, gui = "bold" },
+				b = { fg = colors.black, bg = colors.bright_magenta },
+				c = { fg = colors.black, bg = colors.bright_magenta },
+			},
+			insert = { a = { fg = colors.black, bg = nil } },
+			visual = { a = { fg = colors.black, bg = nil } },
+			replace = { a = { fg = colors.black, bg = nil } },
+			inactive = {
+				a = { fg = colors.white, bg = nil, gui = "bold" },
+				b = { fg = colors.white, bg = nil },
+				c = { fg = colors.white },
+			},
+		}
+	else
+		maple_theme = {
+			normal = {
+				a = { fg = colors.black, bg = colors.white, gui = "bold" },
+				b = { fg = colors.white, bg = colors.brown },
+				c = { fg = colors.white },
+			},
+			insert = { a = { fg = colors.black, bg = colors.blue } },
+			visual = { a = { fg = colors.black, bg = colors.cyan } },
+			replace = { a = { fg = colors.black, bg = colors.red } },
+			inactive = {
+				a = { fg = colors.white, bg = colors.brown, gui = "bold" },
+				b = { fg = colors.white, bg = colors.brown },
+				c = { fg = colors.white },
+			},
+		}
+	end
+
+	local section_separators
+	if transparent then
+		section_separators = { left = "|", right = "|" }
+	else
+		section_separators = { left = "", right = "" }
+	end
+
+	require("lualine").setup({
+		options = {
+			theme = maple_theme,
+			component_separators = "",
+			section_separators = section_separators,
 		},
-		insert = { a = { fg = colors.black, bg = colors.blue } },
-		visual = { a = { fg = colors.black, bg = colors.cyan } },
-		replace = { a = { fg = colors.black, bg = colors.red } },
-		inactive = {
-			a = { fg = colors.white, bg = colors.brown, gui = "bold" },
-			b = { fg = colors.white, bg = colors.brown },
-			c = { fg = colors.white },
+		sections = {
+			lualine_a = { { "mode", separator = { left = section_separators.right }, right_padding = 2 } },
+			lualine_b = { "filename", "branch" },
+			lualine_c = {
+				{ "diagnostics" },
+				"%=", -- center alignment
+			},
+			lualine_x = {
+				{ "diff" },
+			},
+			lualine_y = {
+				{ "filetype" },
+				{ "filesize" },
+				{ "progress" },
+				{ "location", separator = { left = section_separators.right }, left_padding = 2 },
+			},
+			lualine_z = {
+				{ clock, separator = { right = section_separators.left }, left_padding = 2 },
+			},
 		},
-	}
+		inactive_sections = {
+			lualine_a = { "filename" },
+			lualine_b = {},
+			lualine_c = {},
+			lualine_x = {},
+			lualine_y = {},
+			lualine_z = { "location" },
+		},
+		tabline = {},
+		extensions = {},
+	})
 end
 
-local section_separators
-if transparent then
-	section_separators = { left = "|", right = "|" }
-else
-	section_separators = { left = "", right = "" }
-end
+maple.load_lualine = load_lualine
 
-require("lualine").setup({
-	options = {
-		theme = maple_theme,
-		component_separators = "",
-		section_separators = section_separators,
-	},
-	sections = {
-		lualine_a = { { "mode", separator = { left = section_separators.right }, right_padding = 2 } },
-		lualine_b = { "filename", "branch" },
-		lualine_c = {
-			{ "diagnostics" },
-			"%=", -- center alignment
-		},
-		lualine_x = {
-			{ "diff" },
-		},
-		lualine_y = {
-			{ "filetype" },
-			{ "filesize" },
-			{ "progress" },
-			{ "location", separator = { left = section_separators.right }, left_padding = 2 },
-		},
-		lualine_z = {
-			{ clock, separator = { right = section_separators.left }, left_padding = 2 },
-		},
-	},
-	inactive_sections = {
-		lualine_a = { "filename" },
-		lualine_b = {},
-		lualine_c = {},
-		lualine_x = {},
-		lualine_y = {},
-		lualine_z = { "location" },
-	},
-	tabline = {},
-	extensions = {},
-})
+return maple
