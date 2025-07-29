d8eca234cb9210973d7edc4a0986a928c852d17b
Lucas  Galvão <lucasgalvao@Lucass-MacBook-Air.local>
2025-07-29 15:38:05 -0300

    configuring the theme

--- lua/maple/init.lua
+++ lua/maple/init.lua
@@ -6,8 +6,6 @@ function M.setup(opts)
 	require("maple.theme").setup(opts or {}, require("maple.palette").colors(opts.theme or "autumn"))
 	local lualine = require("maple.lualine")
 	lualine.load_lualine(opts.theme)
-	-- Expose the theme globally for LazyVim
-	_G.Snacks = M
 end
 
 return M
