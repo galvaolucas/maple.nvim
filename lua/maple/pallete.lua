95b28d15a801e432a1695ae78bffcaf999dc2a10
Lucas  Galvão <lucasgalvao@Lucass-MacBook-Air.local>
2025-07-28 23:58:21 -0300

    new suggestions

--- lua/maple/pallete.lua
+++ lua/maple/pallete.lua
@@ -1,33 +1,34 @@
 local maple = {}
 
 maple.colors = {
-	bg = "#060606",
-	fg = "#F0EFF4",
-	red = "#DA5552",
-	green = "#BDE4A8",
-	yellow = "#FFFF82",
-	blue = "#89BBFE",
-	magenta = "#C2AFF0",
-	cyan = "#9FFFF5",
-	white = "#FBFBFB",
-	black = "#0F1108",
-	pink = "#FF88DC",
-	columbia_blue = "#B8D0EB",
+	bg = "#0B0C10", -- dark velvet
+	fg = "#EAEAEA", -- old paper parchment
 
-	bright_black = "#1C2321",
-	bright_red = "#ffc4c4",
-	bright_green = "#D7F2BA",
-	bright_yellow = "#F5EE9E",
-	bright_blue = "#A2D6F9",
-	bright_magenta = "#ebe5ff",
-	bright_cyan = "#C9F9FF",
-	bright_white = "#FDFFFC",
+	red = "#E27D60", -- warm coral
+	green = "#A3D9A5", -- soft sage
+	yellow = "#FFE9A3", -- candlelight
+	blue = "#84C5F4", -- alpine sky
+	magenta = "#D7A9E3", -- lilac bloom
+	cyan = "#A6F2E7", -- mint mist
+	white = "#FAFAFA", -- silk veil
+	black = "#1A1B1E", -- graphite shadow
+	pink = "#FFB3D1", -- rose petal
+	columbia_blue = "#C2DFFF", -- cloudwashed sky
 
-	cursor = "#ebe5ff",
-	selection = "#2E294E",
-	comment = "#61707D",
-	border = "#4F4789",
-	status_line = "#343434",
+	bright_black = "#2A2D34", -- smoky ink
+	bright_red = "#FF9999", -- cherry cream
+	bright_green = "#C3F6C6", -- tea leaf
+	bright_yellow = "#FFF5B7", -- morning sun
+	bright_blue = "#A8DCFF", -- glacier
+	bright_magenta = "#F0D7FF", -- soft orchid
+	bright_cyan = "#D4FFFA", -- polar ice
+	bright_white = "#FFFFFF", -- pristine linen
+
+	cursor = "#F0D7FF", -- soft glow
+	selection = "#3C3756", -- moonlight ink
+	comment = "#7A8C98", -- soft graphite
+	border = "#5B537E", -- antique plum
+	status_line = "#2C2C2E", -- iron edge
 }
 
 return maple
