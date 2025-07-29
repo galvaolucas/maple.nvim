6bca8526cab48a38b8b3c934b8edb1ed9505e9a0
Lucas  Galvão <lucasgalvao@Lucass-MacBook-Air.local>
2025-07-29 00:03:31 -0300

    merge

--- lua/maple/theme.lua
+++ lua/maple/theme.lua
@@ -1,8 +1,66 @@
-local maple = require("maple.pallete")
+-- maple.lua – refined version
+
+local maple = {}
+
+local function merge(tbl1, tbl2)
+	for k, v in pairs(tbl2) do
+		tbl1[k] = v
+	end
+end
+
+maple.colors = {
+	bg = "#0B0C10",
+	fg = "#EAEAEA",
+
+	red = "#E27D60",
+	green = "#A3D9A5",
+	yellow = "#FFE9A3",
+	blue = "#84C5F4",
+	magenta = "#D7A9E3",
+	cyan = "#A6F2E7",
+	white = "#FAFAFA",
+	black = "#1A1B1E",
+	pink = "#FFB3D1",
+	columbia_blue = "#C2DFFF",
+
+	bright_black = "#2A2D34",
+	bright_red = "#FF9999",
+	bright_green = "#C3F6C6",
+	bright_yellow = "#FFF5B7",
+	bright_blue = "#A8DCFF",
+	bright_magenta = "#F0D7FF",
+	bright_cyan = "#D4FFFA",
+	bright_white = "#FFFFFF",
+
+	cursor = "#F0D7FF",
+	selection = "#3C3756",
+	comment = "#7A8C98",
+	border = "#5B537E",
+	status_line = "#2C2C2E",
+}
+
 local c = maple.colors
 
 local function hi(name, opts)
 	vim.api.nvim_set_hl(0, name, opts)
+end
+
+local function hi_link(from, to)
+	vim.api.nvim_set_hl(0, from, { link = to })
+end
+
+local function apply_italics()
+	hi("Keyword", { italic = true })
+	hi("@keyword", { italic = true })
+	hi("@keyword.function", { italic = true })
+	hi("@operator", { italic = true })
+	hi("@lsp.type.interface", { italic = true })
+end
+
+local function tsx(name, opts)
+	hi(name, opts)
+	hi(name .. ".tsx", opts)
+	hi(name .. ".typescript", opts)
 end
 
 function maple.setup(opts)
@@ -12,8 +70,8 @@ function maple.setup(opts)
 
 	if transparent then
 		c.bg = "NONE"
-		c.border = "NONE"
-		c.selection = c.selection
+		c.border = c.selection
+		c.status_line = "NONE"
 	end
 
 	vim.cmd("hi clear")
@@ -23,16 +81,18 @@ function maple.setup(opts)
 	vim.o.background = "dark"
 	vim.g.colors_name = "maple"
 
+	-- Core UI
 	hi("Normal", { fg = c.fg, bg = c.bg })
 	hi("Cursor", { fg = c.bg, bg = c.cursor })
 	hi("Visual", { bg = c.selection })
 	hi("LineNr", { fg = c.comment })
 	hi("CursorLineNr", { fg = c.white, bold = true })
-	hi("Comment", { fg = c.comment, italic = true })
-	hi("StatusLine", { fg = c.fg, bg = nil })
+	hi("Comment", { fg = c.comment, italic = italic })
+	hi("StatusLine", { fg = c.fg, bg = c.status_line })
 	hi("VertSplit", { fg = c.border })
 	hi("WinSeparator", { fg = c.border })
 
+	-- Syntax
 	hi("Identifier", { fg = c.cursor })
 	hi("Function", { fg = c.blue })
 	hi("Statement", { fg = c.magenta })
@@ -43,213 +103,57 @@ function maple.setup(opts)
 	hi("Boolean", { fg = c.red })
 	hi("Constant", { fg = c.bright_blue })
 
-	hi("DiagnosticError", { fg = c.red, undercurl = false })
-	hi("DiagnosticWarn", { fg = c.columbia_blue, undercurl = false })
-	hi("DiagnosticInfo", { fg = c.blue, undercurl = false })
-	hi("DiagnosticHint", { fg = c.cyan, undercurl = false })
-	hi("DiagnosticUnderlineError", { fg = c.red, undercurl = true })
-	hi("DiagnosticUnderlineWarn", { fg = c.yellow, undercurl = true })
-	hi("DiagnosticUnderlineInfo", { fg = c.blue, undercurl = false })
-	hi("DiagnosticUnderlineHint", { fg = c.cyan, undercurl = false })
+	-- Diff
+	hi("DiffAdd", { bg = "#193c36" })
+	hi("DiffChange", { bg = "#1c3444" })
+	hi("DiffDelete", { bg = "#3c1e26" })
+	hi("DiffText", { bg = "#284863" })
 
-	hi("NvimTreeNormal", { bg = c.bg })
-	hi("NvimTreeNormalNC", { bg = c.bg })
-	hi("NvimTreeEndOfBuffer", { bg = c.bg })
-	hi("NvimTreeVertSplit", { fg = c.border, bg = c.bg })
+	-- Diagnostics
+	hi("DiagnosticError", { fg = c.red })
+	hi("DiagnosticWarn", { fg = c.yellow })
+	hi("DiagnosticInfo", { fg = c.blue })
+	hi("DiagnosticHint", { fg = c.cyan })
 
+	-- Treesitter (partial)
+	hi("@function", { fg = c.blue })
+	hi("@variable", { fg = c.fg })
+	hi("@property", { fg = c.cyan })
+	hi("@string", { fg = c.green })
+	hi("@number", { fg = c.yellow })
+	hi("@boolean", { fg = c.red })
+
+	-- Telescope
+	hi("TelescopeBorder", { fg = c.border, bg = c.bg })
+	hi("TelescopePromptBorder", { fg = c.comment, bg = c.bg })
+	hi("TelescopePromptTitle", { fg = c.green, bold = true })
+	hi("TelescopeSelection", { fg = c.black, bg = c.green, bold = true })
+
+	-- Pmenu
+	hi("Pmenu", { fg = c.fg, bg = c.bg })
+	hi("PmenuSel", { fg = c.black, bg = c.green })
+	hi("CursorLine", { bg = c.black })
+	hi("ColorColumn", { bg = c.border })
+
+	-- Indent guides
+	hi("IndentBlanklineChar", { fg = c.border })
+	hi("IndentBlanklineContextChar", { fg = c.cyan })
+
+	-- Git signs
 	hi("GitSignsAdd", { fg = c.green })
 	hi("GitSignsChange", { fg = c.blue })
 	hi("GitSignsDelete", { fg = c.red })
 
-	hi("DiffAdd", { bg = "#a4dfae" })
-	hi("DiffChange", { bg = "#8fc7ff" })
-	hi("DiffDelete", { bg = "#edabab" })
-	hi("DiffText", { bg = "#8fc7ff" })
-
-	hi("TelescopeBorder", { fg = c.border, bg = c.bg })
-	hi("TelescopePromptBorder", { fg = c.border, bg = c.bg })
-	hi("TelescopeResultsBorder", { fg = c.border, bg = c.bg })
-	hi("TelescopePreviewBorder", { fg = c.border, bg = c.bg })
-	hi("TelescopePromptNormal", { fg = c.fg, bg = c.bg })
-	hi("TelescopePromptPrefix", { fg = c.green, bg = c.bg })
-	hi("TelescopePromptTitle", { fg = c.green, bg = c.bg, bold = true })
-	hi("TelescopeResultsTitle", { fg = c.fg, bg = c.bg })
-	hi("TelescopePreviewTitle", { fg = c.fg, bg = c.bg })
-	hi("TelescopeSelection", { fg = c.black, bg = c.green, bold = true })
-	hi("TelescopeMultiSelection", { fg = c.green, bg = c.bg })
-	hi("TelescopeMatching", { fg = c.yellow, bold = true })
-
-	hi("@comment", { fg = c.comment, italic = true })
-	hi("@keyword", { fg = c.magenta, bold = true })
-	hi("@keyword.function", { fg = c.magenta })
-	hi("@function", { fg = c.blue })
-	hi("@function.call", { fg = c.blue })
-	hi("@method", { fg = c.blue })
-	hi("@variable", { fg = c.fg })
-	hi("@property", { fg = c.cyan })
-	hi("@field", { fg = c.cyan })
-	hi("@string", { fg = c.green })
-	hi("@number", { fg = c.yellow })
-	hi("@boolean", { fg = c.red })
-	hi("@constant", { fg = c.cyan })
-	hi("@type", { fg = c.yellow })
-	hi("@namespace", { fg = c.magenta })
-	hi("@operator", { fg = c.magenta })
-	hi("@parameter", { fg = c.fg })
-	hi("@punctuation.delimiter", { fg = c.fg })
-	hi("@punctuation.bracket", { fg = c.blue })
-
-	hi("@lsp.type.namespace", { link = "@namespace" })
-	hi("@lsp.type.type", { link = "@type" })
-	hi("@lsp.type.class", { link = "@type" })
-	hi("@lsp.type.enum", { link = "@type" })
-	hi("@lsp.type.interface", { link = "@type" })
-	hi("@lsp.type.struct", { link = "@type" })
-	hi("@lsp.type.parameter", { link = "@parameter" })
-	hi("@lsp.type.variable", { link = "@variable" })
-	hi("@lsp.type.property", { link = "@property" })
-	hi("@lsp.type.enumMember", { link = "@constant" })
-	hi("@lsp.type.function", { link = "@function" })
-	hi("@lsp.type.method", { link = "@method" })
-	hi("@lsp.type.macro", { link = "@function" })
-
-	hi("Pmenu", { fg = c.fg, bg = c.bg })
-	hi("PmenuSel", { fg = c.black, bg = c.green })
-	hi("PmenuSbar", { bg = c.selection })
-	hi("PmenuThumb", { bg = c.selection })
-	hi("CursorLine", { bg = c.black })
-	hi("ColorColumn", { bg = c.border })
-	hi("StatusLineNC", { fg = c.comment, bg = nil })
-	hi("TabLine", { fg = c.comment, bg = c.border })
-	hi("TabLineFill", { bg = c.border })
-	hi("TabLineSel", { fg = c.fg, bg = c.selection })
-	hi("VisualNOS", { fg = c.red, bg = c.bg })
-
-	hi("Search", { bg = c.yellow, fg = c.black })
-	hi("IncSearch", { bg = c.yellow, fg = c.black, bold = true })
-	hi("CursorLineNr", { fg = c.white, bold = true })
-	hi("SnacksDashboardDesc", { fg = c.magenta })
-
-	hi("LazySpecial", { fg = c.bright_magenta })
-	hi("LazyReasonPlugin", { fg = c.magenta })
-
-	vim.api.nvim_create_autocmd("ColorScheme", {
-		pattern = "*",
-		callback = function()
-			vim.api.nvim_set_hl(0, "@type.builtin.tsx", { fg = c.magenta, bold = true })
-			vim.api.nvim_set_hl(0, "@tag.builtin.tsx", { fg = c.bright_red })
-			vim.api.nvim_set_hl(0, "@tag", { fg = c.bright_yellow })
-			vim.api.nvim_set_hl(0, "@tag.tsx", { fg = c.bright_blue, bold = false })
-			vim.api.nvim_set_hl(0, "@spell.tsx", { fg = c.white, bold = false })
-			vim.api.nvim_set_hl(0, "@number.tsx", { fg = c.bright_green, bold = false })
-			vim.api.nvim_set_hl(0, "@type.tsx", { fg = c.bright_yellow, bold = false })
-			vim.api.nvim_set_hl(0, "@boolean.tsx", { fg = c.magenta, bold = true })
-			vim.api.nvim_set_hl(0, "@_jsx_element", { fg = c.cyan, bold = false })
-			vim.api.nvim_set_hl(0, "@_jsx_element.tsx", { fg = c.bright_red, bold = false })
-			vim.api.nvim_set_hl(0, "@tag.delimiter", { fg = c.bright_blue })
-			vim.api.nvim_set_hl(0, "@lsp.type.property.typescriptreact", { fg = c.bright_red })
-			vim.api.nvim_set_hl(0, "@operator.tsx", { fg = c.bright_blue })
-			vim.api.nvim_set_hl(0, "@_jsx_attribute.tsx", { fg = c.bright_yellow })
-			vim.api.nvim_set_hl(0, "@punctuation.bracket.tsx", { fg = c.columbia_blue })
-			vim.api.nvim_set_hl(0, "@keyword.import.tsx", { fg = c.magenta })
-			vim.api.nvim_set_hl(0, "@keyword.tsx", { fg = c.magenta })
-			vim.api.nvim_set_hl(0, "@keyword.typescript", { fg = c.magenta, italic = italic })
-			vim.api.nvim_set_hl(0, "@keyword.exception.typescript", { fg = c.magenta, italic = italic })
-			vim.api.nvim_set_hl(0, "@keyword.exception.tsx", { fg = c.magenta, italic = italic })
-			vim.api.nvim_set_hl(0, "@keyword.operator.typescript", { fg = c.magenta, italic = italic })
-			vim.api.nvim_set_hl(0, "@keyword.operator.tsx", { fg = c.magenta, italic = italic })
-			vim.api.nvim_set_hl(0, "@keyword.coroutine.typescript", { fg = c.magenta, italic = italic })
-			vim.api.nvim_set_hl(0, "@keyword.coroutine.tsx", { fg = c.magenta, italic = italic })
-			vim.api.nvim_set_hl(0, "@keyword.conditional.typescript", { fg = c.magenta, italic = italic })
-			vim.api.nvim_set_hl(0, "@keyword.conditional.tsx", { fg = c.magenta, italic = italic })
-			vim.api.nvim_set_hl(0, "@keyword.return.typescript", { fg = c.magenta, italic = italic })
-			vim.api.nvim_set_hl(0, "@keyword.return.tsx", { fg = c.magenta, italic = italic })
-			vim.api.nvim_set_hl(0, "@lsp.type.variable.typescriptreact", { fg = c.bright_yellow })
-			vim.api.nvim_set_hl(0, "@lsp.type.parameter.typescriptreact", { fg = c.bright_yellow, underline = true })
-			vim.api.nvim_set_hl(0, "@lsp.type.type.typescriptreact", { fg = c.bright_yellow, bold = true })
-			vim.api.nvim_set_hl(0, "@lsp.type.property.typescriptreact", { fg = c.bright_white, bold = false })
-			vim.api.nvim_set_hl(0, "@variable.typescript", { fg = c.bright_yellow, bold = false })
-			vim.api.nvim_set_hl(0, "@variable.parameter.tsx", { fg = c.bright_yellow, bold = false })
-			vim.api.nvim_set_hl(0, "@constructor.typescript", { fg = c.bright_blue, bold = true })
-			vim.api.nvim_set_hl(0, "@lsp.mod.defaultLibrary.typescript", { fg = c.bright_green, bold = false })
-			vim.api.nvim_set_hl(0, "@lsp.mod.declaration.typescript", { fg = c.bright_yellow, bold = false })
-			vim.api.nvim_set_hl(0, "@lsp.mod.local.typescript", { fg = c.bright_yellow, bold = false })
-			vim.api.nvim_set_hl(0, "@lsp.type.namespace.typescriptreact", { fg = c.magenta, bold = true })
-			vim.api.nvim_set_hl(0, "@lsp.type.property.typescriptreact", { fg = c.bright_white, bold = false })
-			vim.api.nvim_set_hl(0, "@lsp.mod.declaration.typescript", { fg = c.bright_blue, bold = false })
-			vim.api.nvim_set_hl(0, "@lsp.type.class.typescript", { fg = c.bright_white, bold = false })
-
-			vim.api.nvim_set_hl(
-				0,
-				"@lsp.typemod.parameter.declaration.typescript",
-				{ fg = c.bright_white, bold = false }
-			)
-
-			vim.api.nvim_set_hl(
-				0,
-				"@lsp.typemod.property.declaration.typescriptreact",
-				{ fg = c.bright_white, bold = false }
-			)
-
-			vim.api.nvim_set_hl(
-				0,
-				"@lsp.type.interface.typescriptreact",
-				{ fg = c.bright_yellow, bold = true, italic = italic }
-			)
-			vim.api.nvim_set_hl(
-				0,
-				"@lsp.typemod.class.defaultLibrary.typescriptreact",
-				{ fg = c.bright_cyan, bold = true }
-			)
-
-			vim.api.nvim_set_hl(0, "@lsp.type.property.lua", { fg = c.magenta, bold = false })
-			vim.api.nvim_set_hl(0, "@property.lua", { fg = c.magenta, bold = false })
-		end,
-	})
-
-	vim.api.nvim_create_autocmd("ColorScheme", {
-		pattern = "maple",
-		callback = function()
-			vim.api.nvim_set_hl(0, "Normal", { bg = c.bg })
-			vim.api.nvim_set_hl(0, "NormalNC", { bg = c.bg })
-			vim.api.nvim_set_hl(0, "NormalFloat", { bg = c.bg })
-			vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "NONE", bg = c.bg })
-
-			-- Sidebars
-			vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = c.bg })
-			vim.api.nvim_set_hl(0, "NvimTreeNormalNC", { bg = c.bg })
-			vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = c.bg })
-			vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = c.bg })
-
-			-- Telescope
-			vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = c.bg })
-			vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = c.bg })
-
-			vim.api.nvim_set_hl(0, "NvimTreeDirectoryName", { fg = c.magenta })
-			vim.api.nvim_set_hl(0, "NvimTreeDirectoryIcon", { fg = c.magenta })
-			vim.api.nvim_set_hl(0, "NeoTreeDirectoryName", { fg = c.magenta })
-			vim.api.nvim_set_hl(0, "NeoTreeDirectoryIcon", { fg = c.magenta })
-		end,
-	})
+	-- TSX/TypeScript refinements
+	tsx("@keyword.operator", { fg = c.magenta, italic = italic })
+	tsx("@keyword.return", { fg = c.magenta, italic = italic })
+	tsx("@type", { fg = c.bright_yellow, bold = false })
+	tsx("@boolean", { fg = c.magenta, bold = true })
+	tsx("@number", { fg = c.bright_green })
 
 	if italic then
-		vim.api.nvim_create_autocmd("ColorScheme", {
-			pattern = "maple",
-			callback = function()
-				vim.api.nvim_set_hl(0, "Keyword", { italic = true })
-				vim.api.nvim_set_hl(0, "@keyword", { italic = true })
-				vim.api.nvim_set_hl(0, "@keyword.function", { italic = true })
-				vim.api.nvim_set_hl(0, "@operator", { italic = true })
-			end,
-		})
+		apply_italics()
 	end
-
-	vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
-		pattern = "*",
-		callback = function()
-			vim.api.nvim_set_hl(0, "Directory", { fg = c.bright_blue })
-		end,
-	})
 end
 
 return maple
